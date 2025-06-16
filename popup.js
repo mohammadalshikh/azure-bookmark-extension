a77069838ab2ad7b5044463dbcc18d536bf87995
Mohammad <95369034+mohammadalshikh@users.noreply.github.com>
2025-06-16 02:51:21 -0400

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,16 +1,19 @@
 document.getElementById("ad-ticket-organizer").addEventListener("click", () => {
     const clientName = document.getElementById("clientName").value.trim();
+    const ticketTitle = document.getElementById("ticketTitle").value.trim();
+    
     if (!clientName) {
         return;
     }
     else {
-        chrome.runtime.sendMessage({ action: "organize-ticket", clientName });
+        chrome.runtime.sendMessage({ action: "organize-ticket", clientName, ticketTitle });
         window.close();
     }
 });
 
 document.addEventListener("DOMContentLoaded", () => {
     const clientNameInput = document.getElementById("clientName");
+    const ticketTitleInput = document.getElementById("ticketTitle");
     const submitButton = document.getElementById("ad-ticket-organizer");
 
     submitButton.disabled = true;
